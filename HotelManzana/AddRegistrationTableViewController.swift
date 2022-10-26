24eea95336f4fa2b2437333199e4812514fc4a31
Ryan Plitt <ryan.plitt@icloud.com>
2022-10-26 09:45:25 -0600

    Added Dates

--- HotelManzana/AddRegistrationTableViewController.swift
+++ HotelManzana/AddRegistrationTableViewController.swift
@@ -13,9 +13,33 @@ class AddRegistrationTableViewController: UITableViewController {
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     
-
+    @IBOutlet weak var checkinDateLabel: UILabel!
+    @IBOutlet weak var checkinDatePicker: UIDatePicker!
+    @IBOutlet weak var checkoutDateLabel: UILabel!
+    @IBOutlet weak var checkoutDatePicker: UIDatePicker!
+    
+    let checkinDatePickerIndexPath = IndexPath(row: 1, section: 1)
+    let checkoutDatePickerIndexPath = IndexPath(row: 3, section: 1)
+    
+    var isCheckinDatePickerVisible: Bool = false {
+        didSet {
+            checkinDatePicker.isHidden = !isCheckinDatePickerVisible
+        }
+    }
+    
+    var isCheckoutDatePickerVisible: Bool = false {
+        didSet {
+            checkoutDatePicker.isHidden  = !isCheckoutDatePickerVisible
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let midnight = Calendar.current.startOfDay(for: Date())
+        checkinDatePicker.minimumDate = midnight
+        checkinDatePicker.date = midnight
     }
     
     @IBAction func doneButtonTapped(_ sender: Any) {
@@ -23,6 +47,34 @@ class AddRegistrationTableViewController: UITableViewController {
         print("First Name: \(String(describing: firstNameTextField.text))")
         print("Last Name: \(String(describing: lastNameTextField.text))")
         print("Email: \(String(describing: emailTextField.text))")
+        print("Check-in: \(checkinDatePicker.date)")
+        print("Check-out: \(checkoutDatePicker.date)")
     }
     
+    func updateDateViews() {
+        checkinDateLabel.text = checkinDatePicker.date.formatted(date: .abbreviated, time: .omitted)
+        
+        checkoutDatePicker.minimumDate = Calendar.current.date(byAdding:
+           .day, value: 1, to: checkinDatePicker.date)
+    }
+    
+    @IBAction func datePickerChanged(_ sender: Any) {
+        updateDateViews()
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case checkinDatePickerIndexPath where isCheckinDatePickerVisible == false: return 0
+        case checkoutDatePickerIndexPath where isCheckoutDatePickerVisible == false: return 0
+        default: return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case checkinDatePickerIndexPath: return 216
+        case checkoutDatePickerIndexPath: return 216
+        default: return UITableView.automaticDimension
+        }
+    }
 }
